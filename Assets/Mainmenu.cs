ceb7721daae39779be970c8183831a04295ee1e1
DepTraiNhatSV <tungvntps41465@gmail.com>
2025-03-03 19:44:46 -0800

    tung

---
+++ Assets/Mainmenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene1");
+        //SceneManager.LoadScene("Scene2");  
+        ////SceneManager.LoadScene("Yang2"); 
+                                        
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Mở cài đặt - Hiển thị UI Options hoặc chuyển scene.");
+   
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Thoát game!");
+        Application.Quit(); 
+    }
+}
